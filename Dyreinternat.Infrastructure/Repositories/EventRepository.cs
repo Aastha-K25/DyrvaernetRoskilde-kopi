49f2aa5edb92b934438fa3cc7e8d8633e97b639d
Aastha-K25 <aak001@edu.zealand.dk>
2025-05-15 22:10:41 +0200

    Blog

--- Dyreinternat.Infrastructure/Repositories/EventRepository.cs
+++ Dyreinternat.Infrastructure/Repositories/EventRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
 using Dyreinternat.Domain.Models;
 using Dyreinternat.Domain.Interfaces;
 
@@ -16,26 +17,30 @@ public class EventRepository : IEventRepository
       e1.Title = "Fælles hundeluftning";
       e1.Description = "Tag med på en hyggelig gåtur med nogle af vores søde og energiske hunde fra Roskilde Dyreværnet. Det er en god måde at lære hundene at kende på, give dem motion og samtidig nyde naturen sammen med andre dyrevenner.";
       e1.Date = DateTime.Today.AddDays(1);
+      e1.ImagePath = "/Images/Hund.png";
 
       Event e2 = new Event();
       e2.Id = 2;
       e2.Title = "Kat efter musen";
       e2.Description = "Kom og vær med til en sjov og hyggelig stund, hvor vi leger med kattene her på Roskilde Dyreværnet. Det er en fantastisk mulighed for at lære kattene at kende, aktivere dem og måske finde dit nye kæledyr.";
       e2.Date = DateTime.Today.AddDays(2);
+      e2.ImagePath = "/Images/Kat.png";
 
       Event e3 = new Event();
-      e2.Id = 3;
-      e2.Title = "Sommerfest";
-      e2.Description = "Deltag i vores årlige sommerfest! Uanset om du er besøgende, frivillig eller bare nysgerrig, er du velkommen til en hyggelig dag med dyr, mad og aktiviteter.";
-      e2.Date = DateTime.Today.AddDays(3);
+      e3.Id = 3;
+      e3.Title = "Sommerfest";
+      e3.Description = "Deltag i vores årlige sommerfest! Uanset om du er besøgende, frivillig eller bare nysgerrig, er du velkommen til en hyggelig dag med dyr, mad og aktiviteter.";
+      e3.Date = DateTime.Today.AddDays(3);
+      e3.ImagePath = "/Images/Sommerfest.png";
 
       Event e4 = new Event();
-      e2.Id = 4;
-      e2.Title = "Lær om adoption og dyrene";
-      e2.Description = "Er du nysgerrig på, hvordan det fungerer at adoptere et dyr? Kom og hør om vores arbejde med katte og hunde, og få indblik i hele adoptionsprocessen. Du får også mulighed for at møde nogle af vores dyr og stille spørgsmål undervejs.";
-      e2.Date = DateTime.Today.AddDays(4);
-
-      
+      e4.Id = 4;
+      e4.Title = "Lær om adoption og dyrene";
+      e4.Description = "Er du nysgerrig på, hvordan det fungerer at adoptere et dyr? " +
+                       "Kom og hør om vores arbejde med katte og hunde, og få indblik i hele adoptionsprocessen. " +
+                       "Du får også mulighed for at møde nogle af vores dyr og stille spørgsmål undervejs.";
+      e4.Date = DateTime.Today.AddDays(4);
+      e4.ImagePath = "/Images/SPG.png";
       
       events.Add(e1);
       events.Add(e2);
