10bfbd6db5e95e5acf54a4825d1af430ae842f76
Aastha-K25 <aak001@edu.zealand.dk>
2025-05-26 22:12:33 +0200

    fiks mergen

--- Dyreinternat.Infrastructure/Repositories/SignUpRepository.cs
+++ Dyreinternat.Infrastructure/Repositories/SignUpRepository.cs
@@ -7,12 +7,8 @@ namespace Dyreinternat.Infrastructure.Repositories;
 public class SignUpRepository : ISignUpRepository
 {
     private static readonly List<SignUp> SignUps = new List<SignUp>();
-
-<<<<<<< HEAD
-    public void Add(SignUp signUp)
-=======
+    
 public void Add(SignUp signUp)
->>>>>>> origin/main
     {
         SignUps.Add(signUp);
     }
