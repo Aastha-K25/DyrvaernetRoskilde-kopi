9fafcebb46e75d7e43b21230a79fa86d360de804
Aastha-K25 <aak001@edu.zealand.dk>
2025-05-28 14:04:56 +0200

    ConsoleApp fiks

--- Dyreinternat.Domain/Models/Adopt.cs
+++ Dyreinternat.Domain/Models/Adopt.cs
@@ -1,14 +1,8 @@
 namespace Dyreinternat.Domain.Models;
 
-public class Adopt
+public class Adopt : Animal
 {
     public int Id { get; set; }  
-    public string Name { get; set; } 
-    public string Breed { get; set; } // Race
-    public string Color { get; set; } 
-    public string Gender { get; set; }
-    public double Weight { get; set; }
-    public int Age { get; set; }
     public bool IsSterilized { get; set; }
     public string Location { get; set; } 
     public bool HasValidPassport { get; set; } // Gyldig pas
---
+++ Dyreinternat.Domain/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace Dyreinternat.Domain.Models;
+
+public class Animal
+{
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Gender { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+}
